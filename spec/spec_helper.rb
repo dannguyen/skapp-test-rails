dd33ee288f2d1f029c62a111882193594816c03d
Dan Nguyen <dansonguyen@gmail.com>
2014-04-14 13:21:53 -0400

    first

---
+++ spec/spec_helper.rb
@@ -0,0 +1,39 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'pry'
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
+
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+   # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+
+  config.before(:each) do
+    DatabaseCleaner.start
+    FactoryGirl.lint
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
